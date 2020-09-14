abb127bbe7d32e1eb51d64cf061948485a48d5f1
MMcClure11 <mcclme02@luther.edu>
2020-09-14 11:02:01 -0500

    formats character cards

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-document.querySelector("p").addEventListener('click', () => {
-    alert("BAM BAMMMMM BAMMM BAMMM BAMMMMMMMM BAMMMMMM")
-} )
 
 const BASE_URL = "http://localhost:3000";
 const CHARACTERS_URL = `${BASE_URL}/characters`;
 const main = document.querySelector("main")
+const charactersDiv = document.createElement("div")
+main.append(charactersDiv)
 
 function fetchCharacters(){
     fetch(CHARACTERS_URL)
@@ -20,11 +19,13 @@ function renderCharacters(json){
 
 function renderCharacter(character){
    let div = document.createElement('div')
+   div.className = "character-card"
    div.innerText = `${character.name}, ${character.species}, ${character.homeworld}`
    let img = document.createElement('img')
    img.src = character.avatar
+   img.className = "character-avatar"
    div.appendChild(img)
-   main.appendChild(div)
+   charactersDiv.appendChild(div)
 }
 
 
